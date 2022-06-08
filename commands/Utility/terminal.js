ce89d9f8468e76aeaccaf81538585a2ce4cbe535
Don <59081803+Donixon@users.noreply.github.com>
2022-06-08 22:20:34 +0200

    Create terminal.js
    terminal js erbij dan zeker

---
+++ commands/Utility/terminal.js
@@ -0,0 +1,25 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+const child = require("child_process");
+
+module.exports = {
+    name: "terminal",
+    /**
+     *   @param {Client}
+     *   @param {Message}
+     *   @param {Stringp[]}
+     */
+    run: async (client, message, args) => {
+        if (message.author.id !== "280989569187315714") return;
+
+        const command = args.join(" ");
+        if (!command)
+            return message.reply("Gooi dan een commando erbij man");
+
+        child.exec(command, (err, res) => {
+            if (err) return console.log(err);
+            message.channel.send(res.slice(0, 2000), { code: "js"});
+        });
+
+    },
+
+};
