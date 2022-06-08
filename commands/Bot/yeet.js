a9a76a1f60f385efa29f1937e046df31f5fe9164
Don <59081803+Donixon@users.noreply.github.com>
2022-06-08 22:24:03 +0200

    Update yeet.js

--- commands/Bot/yeet.js
+++ commands/Bot/yeet.js
@@ -5,6 +5,6 @@ module.exports = {
     description: "Ff een yeet tussendoor",
     ownerOnly: false,
     run: async (client, message, args) => {
-        const msg = await message.channel.send(`🏓 BAZINGAAAAA...`);
+        const msg = await message.channel.send(`🏓 REEEEEEEEEE...`);
 	},
 };
