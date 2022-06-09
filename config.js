bcce451d5e4a8a08c0374e06c4343e16927caa72
Don <59081803+Donixon@users.noreply.github.com>
2022-06-09 08:49:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     botClientID: "961271949491834931",
     botPrefix: "riotbot ",
     ownerID: "280989569187315714",
-    embedColor: "#00dbff",
+    embedColor: "#32803a",
     embedfooterText: "Soms moet een bot ook zwemmen."
 }
