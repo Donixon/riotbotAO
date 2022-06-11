f9386844e7b3ca497ddfe9081c0bc4ad32eadade
leonniesthoven <83581455+leonniesthoven@users.noreply.github.com>
2022-06-11 14:18:20 +0200

    draguntime

--- commands/Bot/draak.js
+++ commands/Bot/draak.js
@@ -8,24 +8,25 @@ module.exports = {
     description: "Even draken spotten",
     ownerOnly: false,
     run: async (client, message, args) => {
-        const msg = await message.channel.send(`wtf is dat een` + this.getRandomDragon());
+        const msg = await message.channel.send(`wtf is dat een` + getRandomDragon());
     },
 
-//make a list of dragon types and return a random dragon
-getRandomDragon: function() {
-    var dragons = ["blauwe draak", "groene draak", "10 koppige demon draak"];
-    var random = dragons[Math.floor(Math.random() * dragons.length)];
-    return random;
-}
+
 
 };
 
   } catch (error) {
 
    
-        run: async (client, message, args) => {
-            const msg = await message.channel.send(error);
+        error.console.log(error);
        
   }
   
-  }
+  
+
+  //make a list of dragon types and return a random dragon
+function getRandomDragon(){
+    var dragons = ["blauwe draak", "groene draak", "10 koppige demon draak"];
+    var random = dragons[Math.floor(Math.random() * dragons.length)];
+    return random;
+}
