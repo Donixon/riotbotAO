23c8135733b1ada17c9bd79c8e54502166b64dba
leonniesthoven <83581455+leonniesthoven@users.noreply.github.com>
2022-06-11 14:08:20 +0200

    traantje

--- commands/Bot/draak.js
+++ commands/Bot/draak.js
@@ -5,15 +5,18 @@ module.exports = {
     description: "Even draken spotten",
     ownerOnly: false,
     run: async (client, message, args) => {
-        const msg = await message.channel.send(`wtf is dat een` + getRandomDragon());
-	},
+        const msg = await message.channel.send(`wtf is dat een` + this.getRandomDragon());
+    },
 
-    //make a list of dragon types and return a random dragon
-    getRandomDragon: function() {
-        var dragons = ["blauwe draak", "groene draak", "10 koppige demon draak"];
-        var random = dragons[Math.floor(Math.random() * dragons.length)];
-        return random;
-    }
+
 
 
 };
+
+
+//make a list of dragon types and return a random dragon
+getRandomDragon: function() {
+    var dragons = ["blauwe draak", "groene draak", "10 koppige demon draak"];
+    var random = dragons[Math.floor(Math.random() * dragons.length)];
+    return random;
+}
