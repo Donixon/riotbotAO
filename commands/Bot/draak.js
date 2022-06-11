7cbbd43c0b85da11d8045415c20e7a476cb26824
leonniesthoven <83581455+leonniesthoven@users.noreply.github.com>
2022-06-11 14:09:30 +0200

    werk

--- commands/Bot/draak.js
+++ commands/Bot/draak.js
@@ -1,3 +1,6 @@
+
+
+
 module.exports = {
     name: "draak",
     aliases: ["dragun", "johandra"],
@@ -7,16 +10,14 @@ module.exports = {
     run: async (client, message, args) => {
         const msg = await message.channel.send(`wtf is dat een` + this.getRandomDragon());
     },
-
-
-
-
-};
-
-
+    
 //make a list of dragon types and return a random dragon
 getRandomDragon: function() {
     var dragons = ["blauwe draak", "groene draak", "10 koppige demon draak"];
     var random = dragons[Math.floor(Math.random() * dragons.length)];
     return random;
 }
+
+};
+
+
