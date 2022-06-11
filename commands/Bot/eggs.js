01b49cea6f80d1fa93f8f4764b6a5d4d33467594
leonniesthoven <83581455+leonniesthoven@users.noreply.github.com>
2022-06-11 17:33:44 +0200

    no message

--- commands/Bot/eggs.js
+++ commands/Bot/eggs.js
@@ -11,16 +11,10 @@ try {
         run: async (client, message, args) => {
             
             
-            try{
+           
             var credits = getCredits();
-            }
-            catch (error){
-                message.channel.send(error);
-            }
-          
-
-          
-
+            
+        
             const msg = await message.channel.send(`Er zitten ` + credits + ` eieren in m'n mandje`);
             
         
@@ -38,10 +32,10 @@ try {
       
       //from stats.xml get Credits from from User by Name
         function getCredits(){
-            var data = fs.readFileSync('./stats.xml', 'utf8');
+            var data = fs.readFileSync('stats.xml', 'utf8');
             var json = parser.toJson(data);
             var obj = JSON.parse(json);
-            var credits = obj.stats.user[0].credits[0]._text;
+            var credits = obj.stats.users.user[0].credits[0]._text;
             return credits;
         }
     
