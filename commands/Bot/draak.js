9aaf5ef8029cd10272cb24b378e4e9de7a7b50d1
leonniesthoven <83581455+leonniesthoven@users.noreply.github.com>
2022-06-11 13:59:05 +0200

    drakendroom

---
+++ commands/Bot/draak.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: "draak",
+    aliases: ["dragun", "johandra"],
+    category: "Utility",
+    description: "Even draken spotten",
+    ownerOnly: false,
+    run: async (client, message, args) => {
+        const msg = await message.channel.send(`wtf is dat een `);
+	},
+
+    //make a list of dragon types and return a random dragon
+    getRandomDragon: function() {
+        var dragons = ["blauwe draak", "groene draak", "10 koppige demon draak"];
+        var random = dragons[Math.floor(Math.random() * dragons.length)];
+        return random;
+    }
+
+
+};
