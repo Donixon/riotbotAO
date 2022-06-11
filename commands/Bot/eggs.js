7106041d96ff301b864a151d8e0c4067a8111f87
leonniesthoven <83581455+leonniesthoven@users.noreply.github.com>
2022-06-11 17:20:05 +0200

    no message

--- commands/Bot/eggs.js
+++ commands/Bot/eggs.js
@@ -9,15 +9,21 @@ try {
         description: "Hoeveel eieren zitter er in m'n mandje",
         ownerOnly: false,
         run: async (client, message, args) => {
+            
+            
+
             const msg = await message.channel.send(`Er zitten ` + getCredits() + ` eieren in m'n mandje`);
+
+        
         },
+        
     
     };
     
       } catch (error) {
        
        console.log(error);
-        
+       message.channel.send(error);
       }
       
       //from stats.xml get Credits from from User by Name
